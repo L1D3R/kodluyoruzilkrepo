bcdf5cf345ab2db52321a90dc22804021b7a8d49
Yeni Kullanıcı Adı <yeni@email.com>
2024-04-26 02:19:08 +0300

    v2

---
+++ src/main/java/git.java
@@ -0,0 +1,19 @@
+public class git {
+    public static void main(String[] args) {
+        // git init komutu localimizde çalışmamızı sağlar
+        // git init komutu o projede tek geçerli sayılır
+        // localde çalışmak istiyorsanız çalışacağınız projeyi localinize almanız gerekir
+        // git status komutu projenin durumunu gösterir
+        // git add . komutu projemizi veya clasımızı geçici alana gönderir
+        // git add . komutundan kullanmadan önce clas isimimiz kırmızı renkte gösterir gönderdikten sonra yeşile döner
+        // git commit -m"v1" komutu ile geçici alandan repositorye göndeririz.
+        int a=8;
+        int b=5;
+        int d=3;
+        System.out.println(a+b);
+        System.out.println("versiyon oluşturuldu");
+        System.out.println(a*b);
+        System.out.println("versiyon 2 oluşturuldu ");
+
+    }
+}
