7d1e2e20d7e8140dd9f104afa9a4ef775170e5a8
Yeni Kullanıcı Adı <yeni@email.com>
2024-04-26 02:50:50 +0300

    v4

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -13,9 +13,6 @@ public class git {
         // git te ana komutlar git init ,git add . , git commit - m"versiyon metni"
         //yardımcı komutlar  git status ,git show ,git log
 
-
-
-
         int a=8;
         int b=5;
         int d=3;
@@ -24,9 +21,14 @@ public class git {
         System.out.println(a*b);
         System.out.println("versiyon 2 oluşturuldu ");
         // version 3
-
         // git restore -- staged komutu ile gecici bölgeye gönderdiğimiz versiyonu geri alır
         System.out.println(a/b);
+        //git restore . komutu ile versiyonun son halinin alır
+        // örneğin v1,v2,v3,v4 oluşturduk v4 doğru düzgün çalışmıyor bunun çözümü olarak v3 dönüp v5 olarak yayınlayıp
+        //v5 olarak kullanıcı kullanır
+        //versiyon 4
+        System.out.println("olmaması gereken kodlar");
+
 
 
     }
